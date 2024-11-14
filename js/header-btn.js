de43487d4412823543a2e8c4679bfcc6649bd2d1
MikhailBLR <124276245+MikhailBLR@users.noreply.github.com>
2024-11-14 22:18:31 +0300

    main

---
+++ js/header-btn.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+function styleOpacity(e,opac) {
+    if(e.target.classList.contains('header__item_link-1761px')){
+        const link = e.target;
+        const innerLinks = document.querySelectorAll('.header__item_link-1761px');
+        
+        innerLinks.forEach((item) => {
+            if(item !== link) {
+                item.style.transition = 'opacity 0.4s'
+                item.style.opacity = this;
+            }
+        });
+    }
+};
+
+const items =document.querySelector('.header__items-1761px');
+items.addEventListener('mouseover',styleOpacity.bind(0.5));
+items.addEventListener('mouseout', styleOpacity.bind(1));
